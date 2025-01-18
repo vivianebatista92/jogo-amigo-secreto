4c5a5692a4bdd72778c42e51c4e9efd15c9bcf78
Viviane Santos <viviane.batista1992@hotmail.com>
2025-01-18 19:10:31 -0300

    Att

--- project/app.js
+++ project/app.js
@@ -1,1 +1,44 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeNomes = [];
+
+const inserirNomes=() => {
+    let nome = document.getElementById('amigo').value;
+    if(nome === '') {
+        alert('Insira o nome do amigo, por favor!');
+        return;  
+    } else {
+        listaDeNomes.push(nome);
+    }
+    listarNomes();
+    limparCampo();
+    limparSorteado();
+
+}
+
+function listarNomes() {
+    let listaDeExibicao = document.getElementById('listaAmigos');
+    let tagItem
+    for(let I = 0; I < listaDeNomes.length; I++){
+        tagItem = document.createElement('li');
+        tagItem.innerHTML = listaDeNomes[I];
+
+    }
+listaDeExibicao.appendChild(tagItem);
+}
+
+function limparCampo() {
+    nome = document.getElementById('amigo');
+    nome.value = '';
+}
+
+function sortearAmigo() {
+    let sorteio = document.getElementById('resultado');
+    let amigoSorteado = listaDeNomes[Math.floor(Math.random() * listaDeNomes.length)];
+    let tagSorteio = document.createElement('li');
+    tagSorteio.innerHTML = amigoSorteado;
+    sorteio.appendChild(tagSorteio);
+}
+
+function limparSorteado(){
+    sorteio = document.getElementById('resultado');
+    sorteio.innerTEXT = '';
+}
