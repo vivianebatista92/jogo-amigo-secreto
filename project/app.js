4ada93ef98443d246a4ffe015f45cab47c7e4b54
Viviane Santos <viviane.batista1992@hotmail.com>
2025-01-20 20:15:08 -0300

    Update app.js

--- project/app.js
+++ project/app.js
@@ -3,15 +3,26 @@ let listaDeNomes = [];
 const inserirNomes=() => {
     let nome = document.getElementById('amigo').value;
     if(nome === '') {
-        alert('Insira o nome do amigo, por favor!');
+        window.alert('Insira o nome do amigo, por favor!');
         return;  
+    } 
+    
+    if(nome.length < 3) {
+        window.alert('O nome do amigo deve ter no mínimo 3 caracteres');
+        return false;
+    } 
+    
+    if(listaDeNomes.includes(nome)){
+        window.alert('Esse amigo já foi adicionado!');
+        return false;
+     
     } else {
         listaDeNomes.push(nome);
     }
     listarNomes();
     limparCampo();
     limparSorteado();
-
+    
 }
 
 function listarNomes() {
@@ -33,12 +44,20 @@ function limparCampo() {
 function sortearAmigo() {
     let sorteio = document.getElementById('resultado');
     let amigoSorteado = listaDeNomes[Math.floor(Math.random() * listaDeNomes.length)];
-    let tagSorteio = document.createElement('li');
-    tagSorteio.innerHTML = amigoSorteado;
+    let tagSorteio = document.createElement('div');
+    tagSorteio.innerHTML = `Parabéns, o sorteado foi ${amigoSorteado}`;
     sorteio.appendChild(tagSorteio);
+    limparListaDeNomes();
+    
 }
 
 function limparSorteado(){
     sorteio = document.getElementById('resultado');
-    sorteio.innerTEXT = '';
+    sorteio.innerText = '';
+}
+
+function limparListaDeNomes(){
+    let listaDeExibicao = document.getElementById('listaAmigos');
+    listaDeNomes = [];
+    listaDeExibicao.innerText= '';
 }
